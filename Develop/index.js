6aae257d98819a477c3978a0b42c223a0980eba5
kristermyr <krister90@gmail.com>
2022-12-24 09:40:06 -0600

    added questions for information to readme

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,37 @@
 // TODO: Include packages needed for this application
-
+const fs = require('fs');
+const inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [];
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your name?',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'Type in your bio:',
+      name: 'bio',
+    },
+    {
+      type: 'url',
+      message: 'Enter your Linkdn url',
+      name: 'Linkdn',
+    },
+    {
+        type: 'url',
+        message: 'Enter your Github url',
+        name: 'Github',
+      },
+  ])
+  .then((response) =>
+    fs.writeFile('index.html', response.name,(error,data) =>
+    error ? console.error(error) : console.log(data)
+    )
+  );
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
